d0e8d6570725c1c4515f31b8b4190e170d044928
Stephane <slevrai@yahoo.fr>
2019-02-17 23:18:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header.vue'
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+import singleBlog from './components/singleBlog.vue'
+
+export default {
+  components: {
+    'app-header': header,
+    'add-blog': addBlog,
+    'show-blogs': showBlogs,
+    'single-blog': singleBlog
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+#app{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
